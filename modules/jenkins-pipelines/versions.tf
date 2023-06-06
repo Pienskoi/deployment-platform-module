836a8d28af7f203c78b8d3efb2d168179e84c669
Pienskoi <vladymyrpenskoy@gmail.com>
2023-06-07 01:05:13 +0300

    Add required providers

---
+++ modules/jenkins-pipelines/versions.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.67.0"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.67.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.21.1"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.10.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
