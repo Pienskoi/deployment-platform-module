cab4453e25cc85b165b176de932ecc1f65e87dff
Pienskoi <vladymyrpenskoy@gmail.com>
2023-06-07 22:24:21 +0300

    Update example outputs

--- examples/infrastructure/outputs.tf
+++ examples/infrastructure/outputs.tf
@@ -1,5 +1,6 @@
 output "wireguard_server_public_ip" {
-  value = module.deployment_platform.wireguard_server_public_ip
+  description = "Wireguard server public IP address"
+  value       = module.deployment_platform.wireguard_server_public_ip
 }
 
 output "wireguard_client_private_key" {
@@ -11,4 +12,14 @@ output "wireguard_client_private_key" {
 output "wireguard_server_public_key" {
   description = "Wireguard server public key"
   value       = module.deployment_platform.wireguard_server_public_key
+}
+
+output "cluster_endpoint" {
+  value = module.deployment_platform.cluster_endpoint
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value = module.deployment_platform.cluster_ca_certificate
+  sensitive = true
 }
