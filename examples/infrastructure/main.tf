fd42efc76dd16ae79e9526965d7e3d1e0fada42b
Pienskoi <vladymyrpenskoy@gmail.com>
2023-06-07 22:30:13 +0300

    Update examples

--- examples/infrastructure/main.tf
+++ examples/infrastructure/main.tf
@@ -10,26 +10,7 @@ provider "google-beta" {
   zone    = var.zone
 }
 
-module "project_services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 14.2"
-
-  project_id = var.project_id
-
-  activate_apis = [
-    "compute.googleapis.com",
-    "container.googleapis.com",
-    "storage-api.googleapis.com",
-    "sqladmin.googleapis.com",
-    "servicenetworking.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "iam.googleapis.com",
-    "dns.googleapis.com",
-    "artifactregistry.googleapis.com"
-  ]
-}
-
-module "deployment_platform" {
+module "deployment_infrastructure" {
   source = "../../terraform"
 
   project_id        = var.project_id
